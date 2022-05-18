cb04541f8edc63598e9aac0f7c09d1a8dab75096
Testing Project <kanunulanga@gmail.com>
2022-05-18 13:37:47 +0200

    Changes

---
+++ SpringBootTesting-c7d603f954eeab1b3ac8661f8b646cbc67f4f7f4/src/test/java/com/udemy/spring/springselenium/SpringSeleniumApplicationTests.java
@@ -0,0 +1,41 @@
+package com.udemy.spring.springselenium;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeleniumApplicationTests {
+
+
+
+}
+
+//user.printDetails();                                          //When printing the details.
+//System.out.println(this.path);                               // Accessing the part
+//System.out.println(this.fruits);                                //Accessing the properties in application.properties file.
+//this.user.printDetails();
+
+//__________________________________________________________________________________________________________________________
+
+/*
+Address address=new Address();
+Salary salary=new Salary();
+User user=new User(address,salary);
+user.printDetails();
+*/
+
+//___________________________________________________________________________________________________________________________
+
+/*
+//When accessing the path is not running
+	@Value("PATH")
+	private String path;
+
+//if you want to access property that is in application.properties file.
+	@Value("${fruits}")
+	private String fruits;
+
+ */
